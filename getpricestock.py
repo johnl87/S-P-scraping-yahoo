30ed8452b1700b02370fcb6671b2d096f9c66792
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-08 15:08:59 -0500

    displays stock prices with change

--- getpricestock.py
+++ getpricestock.py
@@ -2,14 +2,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
+def getData(symbol):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
 
-url = 'https://ca.finance.yahoo.com/quote/%5EGSPC'
+    url = f'https://ca.finance.yahoo.com/quote/{symbol}'
 
-r = requests.get(url)
-#print(r.status_code)
+    r = requests.get(url, headers= headers)
+    #print(r.status_code)
 
-soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    stock = {
+        'price': soup.find('div', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text,
+        'change': soup.find('div', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[1].text,
+        'percentChange': soup.find('div', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[2].text
+    }
+    return stock
+    #price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
+    #change = soup.find('fin-streamer', {'class': 'Fw(500) Pstart(8px) Fz(24px)'}).text
 
-price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
-print(price)
+print(getData('%5EGSPC'))
