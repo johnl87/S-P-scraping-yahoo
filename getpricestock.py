a4d1b54623ec3f69242d7bcb6c3a3fd98762fa25
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-08 19:22:19 -0500

    added a way to display stock data to json format

--- getpricestock.py
+++ getpricestock.py
@@ -1,6 +1,11 @@
-
+#importing libraries
 import requests
 from bs4 import BeautifulSoup
+import json
+
+#S&P500, S&P/TSX comp index, Nasdaq
+listOfStocks = ['%5EGSPC', 'NQ%3DF', '%5EGSPTSE']
+displayStocks = []
 
 def getData(symbol):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
@@ -11,13 +16,20 @@ def getData(symbol):
     #print(r.status_code)
 
     soup = BeautifulSoup(r.text, 'html.parser')
+    #using stock dictionary
     stock = {
         'price': soup.find('div', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text,
         'change': soup.find('div', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[1].text,
         'percentChange': soup.find('div', {'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[2].text
     }
     return stock
-    #price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
-    #change = soup.find('fin-streamer', {'class': 'Fw(500) Pstart(8px) Fz(24px)'}).text
 
-print(getData('%5EGSPC'))
+
+#runs a for each loop that iterates each item inside list of stocks and displays the stock details at the current time
+for stocks in listOfStocks:
+    displayStocks.append(getData(stocks))
+    print('Obtaining... please wait', stocks)
+
+#outputs the list of stock data in json format
+with open ('yahooStocks.json', 'w') as f:
+    json.dump(displayStocks, f)
